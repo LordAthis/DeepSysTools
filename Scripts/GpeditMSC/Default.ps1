3665e7f43e6169d952ea173f5f6e04d5fefa69ca
LordAthis <lordathis@gmail.com>
2026-05-06 04:22:40 +0200

    Fix Hungarian language messages in Default.ps1

--- Scripts/GpeditMSC/Default.ps1
+++ Scripts/GpeditMSC/Default.ps1
@@ -2,25 +2,22 @@
 $GpPath = "$env:SystemRoot\System32\gpedit.msc"
 $IsHome = (Get-WmiObject Win32_OperatingSystem).Caption -like "*Home*"
 
-# 1. Lekérdezzük, hogy létezik-e már
 if (Test-Path $GpPath) {
-    Write-Host "A Csoportházirend elérhető. Indítás..." -ForegroundColor Green
+    Write-Host "A Csoporthazirend elerheto. Inditas..." -ForegroundColor Green
     Start-Process "gpedit.msc"
 } else {
     if ($IsHome) {
-        Write-Host "A rendszered Home verzió, alapból nincs rajta Gpedit." -ForegroundColor Yellow
-        $Ans = Read-Host "Szeretnéd aktiválni a rendszertulajdonságot? (I/N)"
+        Write-Host "A rendszered Home verzio, alapbol nincs rajta Gpedit." -ForegroundColor Yellow
+        $Ans = Read-Host "Szeretned aktivalni a rendszertulajdonsagot? (I/N)"
         if ($Ans -eq "i") {
-            Write-Host "Aktiválás folyamatban... Ez eltarhat egy ideig." -ForegroundColor Cyan
-            # DISM alapú engedélyezés
+            Write-Host "Aktivalas folyamatban... Ez eltarhat egy ideig." -ForegroundColor Cyan
+            # DISM alapú aktiválás a Home kiadásokhoz
             Get-ChildItem "$env:SystemRoot\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum" | ForEach-Object { dism /online /norestart /add-package:"$_" }
             Get-ChildItem "$env:SystemRoot\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum" | ForEach-Object { dism /online /norestart /add-package:"$_" }
-            
-            Write-Host "Kész! Indítás..." -ForegroundColor Green
+            Write-Host "Kesz! Inditas..." -ForegroundColor Green
             Start-Process "gpedit.msc"
         }
     } else {
-        Write-Host "Hiba: A gpedit.msc nem található, pedig a rendszer nem Home!" -ForegroundColor Red
-        pause
+        Write-Host "Hiba: A gpedit.msc nem talalhato!" -ForegroundColor Red
     }
 }
